b7503bb9be6071aebed974b0468781f5774d54ea
saha-KUSHAL <krbfxofficial@gmail.com>
2022-12-30 19:46:38 +0530

    reworked on search_student_data

--- src/search_student_data.c
+++ src/search_student_data.c
@@ -3,34 +3,44 @@
 student_data *data;
 void search_student_data(long int no)
 {
-    FILE *fp = fopen("files/student.dat", "r");
-    if (fopen("files/student.dat", "r") == NULL)
+    FILE *fp;
+    int ch = 0;
+    if (mt_file("files/filelist.txt"))
     {
-        red();
-        printf("\nError!");
-        reset();
-    }
-    else if (mt_file("files/student.dat"))
-    {
-        red();
-        printf("\nNo Data.");
-        reset();
+        printf("\nError or File not available.");
     }
     else
     {
-        data = (student_data *)malloc(sizeof(student_data));
-        rewind(fp);  
-        fread(data, sizeof(student_data), 1, fp);
-        while (!feof(fp))
+        stream_list *fname = (stream_list *)malloc(sizeof(stream_list));
+        fp = fopen("files/filelist.txt", "r");
+        rewind(fp);
+        fread(fname, sizeof(stream_list), 1, fp);
+        while (!feof(fp) && ch == 0)
         {
-            if (data->reg_no == no)
+            if (mt_file(fname->path))
+                fread(fname, sizeof(stream_list), 1, fp);
+            else
             {
-                printf("\nReg no.              Name.           Stream         Semester");
-                printf("\n%12ld           %10s       %4s      %s", data->reg_no, data->name, data->stream, data->sem);
+                FILE *fp2 = fopen(fname->path, "r");
+                student_data *data = (student_data *)malloc(sizeof(student_data));
+                rewind(fp2);
+                fread(data, sizeof(student_data), 1, fp2);
+                while (!feof(fp2) && ch == 0)
+                {
+                    if (data->reg_no == no)
+                    {
+                        printf("\nReg.no      Name      Stream      Semester     Year    Total Marks    Grade");
+                        printf("%ld    %s    %s    %s  %s    %d %c", data->reg_no, data->name, data->stream, data->sem, data->year, data->marks, data->grade);
+                        ch = 1;
+                    }
+                    else
+                        fread(data, sizeof(student_data), 1, fp2);
+                }
+                fclose(fp2);
+                free(data);
             }
-                fread(data, sizeof(student_data), 1, fp);
+            fread(fname, sizeof(stream_list), 1, fp);
         }
-        free(data);
+        fclose(fp);
+        free(fname);
     }
-    fclose(fp);
-}
